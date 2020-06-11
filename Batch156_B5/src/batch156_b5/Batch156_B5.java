8efa9309589508a092c17298b77c501a91c25819
Hagimoli <hagimoli0123@gmail.com>
2020-06-11 15:05:36 +0700

    Add new folder Batch156_B5

---
+++ Batch156_B5/src/batch156_b5/Batch156_B5.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package batch156_b5;
+
+import java.io.Console;
+
+/**
+ *
+ * @author Hi-XV
+ */
+public class Batch156_B5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        Console cons = System.console();
+//        if(cons == null){
+//            System.out.println("ko co console");
+//            return;
+//        }
+//        try {
+//            
+//        } catch (Exception e) {
+//        }
+
+
+    }
+    
+}
